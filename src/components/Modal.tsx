12e3f861b21854b5c6dcc5ae7c477078a0ac5baa
icm213 <marekpownuk@gmail.com>
2023-01-25 13:04:23 +0100

    work on modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,16 +1,30 @@
 import React from "react";
-import { tableProduct } from "../interfaces/tableProduct";
+import { handleProductName } from "../methods/handleProductName";
+
+import { passDataToModal } from "../interfaces/passDataToModal";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
-const Modal: React.FC<tableProduct> = (props) => {
+const Modal: React.FC<passDataToModal> = (props) => {
   return (
     <div className="modal--bg">
       <div className="modal--close">
-        <CloseRoundedIcon />
+        <CloseRoundedIcon onClick={props.toggleModal} />
       </div>
-      <div className="modal">
+      <div
+        className="modal"
+        style={{
+          backgroundImage: `linear-gradient(45deg, #ddd, ${props.color}80, #ddd)`,
+        }}
+      >
         <ul>
+          <li>Name:</li>
+          <li>ID:</li>
+          <li>Year:</li>
+          <li>Color:</li>
+          <li>Pantone value:</li>
+        </ul>
+        <ul>
+          <li> {handleProductName(props.name)}</li>
           <li>{props.id}</li>
-          <li>{props.name}</li>
           <li>{props.year}</li>
           <li>{props.color}</li>
           <li>{props.pantone_value}</li>
---
+++ src/methods/handleProductName.ts
@@ -0,0 +1,8 @@
+export const handleProductName = (name: string) => {
+  const splitted = name.split(" ");
+  for (let i = 0; i < splitted.length; i++) {
+    splitted[i] = splitted[i][0].toUpperCase() + splitted[i].slice(1);
+  }
+  const joined = splitted.join(" ");
+  return joined;
+};
